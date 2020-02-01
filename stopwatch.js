5d7b365c74bc553908d90afaa344f6bab96ea9e1
bhoj bahadur karki <nishantkarki305@gmail.com>
2020-02-01 16:29:31 +0545

    refactored stopwatch exercise

--- stopwatch.js
+++ stopwatch.js
@@ -5,47 +5,36 @@ unit used is in seconds
 */
 
 function Stopwatch(){
-    let starttime = 0;
-    let stoptime = 0;
-    let timer_start_status = false;
-    let timer_stop_status = false;
+    let starttime = 0, stoptime = 0, running = false;
+
     this.start = function(){
-        if (timer_start_status == false){
+        if (running == false){
             starttime = new Date().getTime();
             // reset
-            timer_start_status = true;
-            timer_stop_status = false;
+            running  = true;
         }else{
             throw new Error('Timer already started')
         }
         
     }
     this.stop = function(){
-        if (timer_stop_status == false ){
+        if (running == true){
             stoptime = new Date().getTime();
-
             // reset timer status
-            timer_start_status = false;
-            timer_stop_status = true;
+            running = false
         }else{
             throw new Error('Timer already stopped')
         }
     }
-    Object.defineProperty(this,'defaultDuration',{
+    Object.defineProperty(this,'duration',{
         get: function(){
-            if (timer_start_status == false && timer_stop_status == true){
-                return (stoptime - starttime)/3600 +"seconds"
-            }else{
-                return "Start and stop the timer first!"
-            }
-           
+            return (stoptime - starttime)/1000 +" seconds"
         }
     })
 
     this.reset =  function(){
         starttime = 0;
         stoptime = 0;
-        timer_start_status = false;
-        timer_stop_status = false;
+        running = false;
     }
 }
